9972de0996e9f6f265774186ff40434135abcda2
Yash Rawat <yashrawat225@gmail.com>
2020-12-19 18:26:08 +0530

    10 commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ecommercestoreheroku.herokuapp.com/api'
+  // apiUrl: 'https://ecommercestoreheroku.herokuapp.com/api'
+  apiUrl: `http://localhost:3000/api`
 };

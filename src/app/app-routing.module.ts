c136063012f041e21322b6508453549eb2dbb9f5
Yash Rawat <yashrawat225@gmail.com>
2020-12-30 19:04:29 +0530

    fixed cart min quantity validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { ContactUsComponent } from './utils/contact-us/contact-us.component';
 import { FaqsComponent } from './utils/faqs/faqs.component';
-// import { HomepageComponent } from './utils/homepage/homepage.component';
 import { PagenotfoundComponent } from './utils/pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
-  // { path: 'homepage', component: HomepageComponent },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(a => a.AuthModule), data: { preload: true } },
   { path: 'product', loadChildren: () => import('./product/product.module').then(p => p.ProductModule), canActivate: [AuthGuard] },
   { path: 'user', loadChildren: () => import('./user/user.module').then(u => u.UserModule), canActivate: [AuthGuard] },
